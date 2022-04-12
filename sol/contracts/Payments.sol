2567f8c4fe0ad94d996529a2484d0af18fb7fbc3
Aleksei <kilroygood@gmail.com>
2022-04-12 13:41:41 +0300

    payments

--- sol/contracts/Payments.sol
+++ sol/contracts/Payments.sol
@@ -4,5 +4,16 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract payments {
-    
+    uint carrentBalance;
+
+    function receivFunds() external payable {
+        carrentBalance = carrentBalance + msg.value;
+    }
+    function getBalance() public view returns(uint) {
+        return carrentBalance;
+    }
+    function withdrawFunds(address payable _to) external {
+        _to.transfer(carrentBalance);
+        carrentBalance = 0;
+    }
 }
