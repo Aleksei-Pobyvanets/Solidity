f0adc3cbe0eb5d1619f7d194d457b6500dccd40d
Aleksei <kilroygood@gmail.com>
2022-04-12 16:54:37 +0300

    MoreRequires

--- sol/contracts/ArrayStruct.sol
+++ sol/contracts/ArrayStruct.sol
@@ -5,31 +5,39 @@ contract ArrayStruct {
 
     enum Statuses {Unpayed, Payed, Shipped}
 
-    Statuses status;
-
-    function check() public { //проверяет статус
-        status = Statuses.Payed;
-        require(status == Statuses.Payed, "Status wrong");
-        status = Statuses.Shipped; 
-    }
-
-    struct Payment { // структура, в ней храниться история транзакций.
+    struct Payment {
         uint amount;
         uint timestamp;
     }
 
-    Payment[] public payments; //обьявление для простого использования
+    uint public takenEth;
 
-    function simple() public payable{ // Функция для закидывания на контракт и записи в историю и показывания её
+    Payment[] public payments;
+
+    Statuses status;
+
+    function check() public {  //Проверить статус
+        require(status == Statuses.Payed, "Not norm stat");
+    }
+
+    function payFunc() public payable{ //проверка на пустоту транзы и запись в истоиию
+        takenEth = msg.value;
+        require(takenEth > 0, "Empty");
         Payment memory payment = Payment(msg.value, block.timestamp);
         payments.push(payment);
     }
 
-    function withdrawAll(address payable _to) public{ // Функция для снятия сразу всеё суммы с контракта, со всех записей за раз
+    function withdrawAll(address payable _to) public { // вывод с проверкой полная ли стоемость оплачена
         uint total;
         for(uint i = 0; i < payments.length; i++){
             total += payments[i].amount;
         }
-        _to.transfer(total);
+        require(total >= 5 ether, "Pay More Stupid");
+            _to.transfer(total);
+            status = Statuses.Payed;
+    }
+
+    function getBalance() public view returns(uint){ //Проверка баланса
+        return address(this).balance;
     }
 }
