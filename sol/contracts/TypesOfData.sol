83e3a7a8920f0065ea851005e5927b56647f7fbb
Aleksei <kilroygood@gmail.com>
2022-04-14 13:46:35 +0300

    TypesOfData

---
+++ sol/contracts/TypesOfData.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+
+contract TypesOfData {
+
+    address public addrZ;
+
+    function newStr(address newaddr) public {
+        addrZ = newaddr;
+    }
+    
+}
+   
