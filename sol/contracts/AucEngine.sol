1338d9b7203bd4c904612e566d7cfc3cbe9e3c0b
Aleksei <kilroygood@gmail.com>
2022-04-16 15:36:18 +0300

    Add

--- sol/contracts/AucEngine.sol
+++ sol/contracts/AucEngine.sol
@@ -9,7 +9,7 @@ contract AucEngine {
     struct Auction {
         address payable seller;
         uint startingPrice;
-        uint sartAt;
+        uint startAt;
         uint endsAt;
         uint discauntRate;
         string item;
@@ -18,7 +18,30 @@ contract AucEngine {
 
     Auction[] public auctions;
 
+    event AuctionCreated(uint index, string itemName, uint startingPrice, uint duration);
+
     constructor() {
         owner = msg.sender;
+    }
+
+
+    function createAuction(uint _startingPrice, uint _discountRate, string calldata _item, uint _duration) external {
+        uint duration = _duration == 0 ? DURATION : _duration; //if else 
+
+        require(_startingPrice >= _discountRate * duration, "Incorect price");
+
+        Auction memory newAuction = Auction({
+            seller: payable(msg.sender),
+            startingPrice: _startingPrice,
+            finalPrice: _startingPrice,
+            discountRate: _discountRate,
+            startAt: block.timestamp,
+            endsAt: block.timestamp + duration,
+            item: _item,
+            stoped: false
+        });
+        auctions.push(newAuction);
+
+        emit AuctionCreated(auctions.length - 1, _item, _startingPrice, _duration);
     }
 }
