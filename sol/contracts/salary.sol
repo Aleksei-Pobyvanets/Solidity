a9f8765a11c8ba101f779b1df9cc92e8e983228e
Aleksei <kilroygood@gmail.com>
2022-04-20 16:36:52 +0300

    add

--- sol/contracts/salary.sol
+++ sol/contracts/salary.sol
@@ -25,6 +25,9 @@ contract paySalary {
 
     Sal[] public sals;
 
+    // event WorkerCreated(uint index, string itemName, uint startingPrice, uint duration);
+    // event WorkerTakesSalary(uint index, uint finalPrice, address winner);
+
     constructor() {
         owner = msg.sender;
     }
@@ -41,7 +44,9 @@ contract paySalary {
 
         uint medDays = _medicDays > 32 ? _medicDays - 32: _medicDays;
 
-
+        // uint totPay = calcTotalToPay(_workedHours, _salForHour, _absentDAys, medDays, _medicDays);
+        // uint totAbs = calcAbs(_absentDAys, _medicDays, medDays);
+        // uint totAbsDays = allAbsDaysFunc(_absentDAys, _medicDays);
 
         Sal memory newWorkerSal = Sal({
             workerName: _workerName,
@@ -57,9 +62,13 @@ contract paySalary {
         });
 
         sals.push(newWorkerSal);
+
+        // emit WorkerCreated(sals.length - 1, _worker, , duration); 
+
+        pay(_worker, calcTotalToPay(_workedHours, _salForHour, _absentDAys, medDays, _medicDays));
     }
 
-    function calcAbs(uint _absentDAys, uint _medicDays, uint medDays) public returns(uint){
+    function calcAbs(uint _absentDAys, uint _medicDays, uint medDays) private returns(uint){
         if(_medicDays > 32){
             uint absDays = _absentDAys + medDays;
             return absDays;
@@ -67,27 +76,36 @@ contract paySalary {
         return _absentDAys;
     }
 
-    function calcTotalToPay(uint _workedHours, uint _salForHour, uint _absentDAys, uint medDays, uint _medicDays) public returns(uint){
+    function calcTotalToPay(uint _workedHours, uint _salForHour, uint _absentDAys, uint medDays, uint _medicDays) private returns(uint){
         uint perem = calcAbs(_absentDAys, _medicDays, medDays);
         uint total = _salForHour * _workedHours;
         if(perem > 20){
             total = _salForHour * (_workedHours - (perem - 20));
-            // require(total <= 0, "error");
             return total;
         } else {
             return total;
         }
     }
 
-    function allAbsDaysFunc(uint _absentDAys, uint _medicDays) public returns(uint){
+    function allAbsDaysFunc(uint _absentDAys, uint _medicDays) private returns(uint){
         uint allAbsDays = _absentDAys + _medicDays;
         return allAbsDays;
     }
 
-    function checkWorkers() public view returns(uint){
+
+    function checkWorkers() private view returns(uint){
         for(uint i = 0;i < sals.length; i++){
             return sals.length;
         }
     }
 
+    function pay(address payable worker, uint toPaySal) public payable{
+        worker.transfer(toPaySal);
+    }
+
+    function takeEther() public payable {
+    }
+    function chackEther() public view returns(uint){
+        return address(this).balance;
+    }
 }
